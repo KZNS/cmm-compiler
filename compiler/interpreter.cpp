92727584483329ffb55411be2bba6bd6f52ccbc3
Nathan <2889205153@qq.com>
2020-12-15 22:36:38 +0800

    feat(interpreter):add jmp & func

--- compiler/interpreter.cpp
+++ compiler/interpreter.cpp
@@ -6,6 +6,8 @@
 #include <sstream>
 #include <string>
 #include <vector>
+#include <unordered_map>
+#include <algorithm>
 using namespace std;
 void SplitString(const std::string& s, std::vector<std::string>& v, const std::string& c)
 {
@@ -33,8 +35,10 @@ string& trim(string &s)
     return s;
 }
 PcodeInterpreter::PcodeInterpreter(){
-    this->eip = 0;
-    this->old_sp = 0;
+    unordered_map<string,int> m;
+    this->eip.push(0);
+    this->old_sp.push(0);
+    this->runtimeVarLookup.push(m);
     this->cmdHandler["var"] = do_var;
     this->cmdHandler["push"] = do_push;
     this->cmdHandler["pop"] = do_pop;
@@ -42,26 +46,116 @@ PcodeInterpreter::PcodeInterpreter(){
     this->cmdHandler["mul"] = do_mul;
     this->cmdHandler["print"] = do_print;
     this->cmdHandler["input"] = do_input;
+    this->cmdHandler["jmp"] = do_jmp;
+    this->cmdHandler["jz"] = do_jz;
+    this->cmdHandler["exit"] = do_exit;
 }
 //add / sub / mul / div / mod / cmpeq / cmpne / cmpgt 
 // cmplt / cmpge / cmple / and / or / not / neg
+int PcodeInterpreter::do_arg(const string cmd){
+    vector<string> args;
+    unordered_map<string,int> m;
+    runtimeVarLookup.push(m);
+    SplitString(cmd,args,",");
+    if(runtimeStack.size()<args.size()){
+        cout << "invalid stack depth in do_Args"<<endl;
+        return -1;
+    }
+    reverse(args.begin(),args.end());
+    for(auto a:args){
+        runtimeVarLookup.top()[a] = runtimeVar.size();
+        runtimeVar.push_back({"arg",a,runtimeStack.top()});//type,name,val
+        runtimeStack.pop();
+    }
+    return 0;
+}
+int PcodeInterpreter::do_ret(const string cmd){
+    if(cmd=="~"){
+        int t = runtimeStack.top();
+        int n = old_sp.top();
+        old_sp.pop();
+        int o = old_sp.top();
+        for(int i=n;i>o;i--){
+            runtimeVar.pop_back();
+        }
+        runtimeVarLookup.pop();
+        runtimeStack.push(t);
+        //需要pop和push里记录用了几次，最后在这里运行时栈
+        eip.pop();
+        return 0;
+    } else if(cmd==""){
+        int n = old_sp.top();
+        old_sp.pop();
+        int o = old_sp.top();
+        for(int i=n;i>o;i--){
+            runtimeVar.pop_back();
+        }
+        runtimeVarLookup.pop();
+        //pop everything in this func and return
+        eip.pop();
+        return 0;
+    } else if(runtimeVarLookup.top().find(cmd)!=runtimeVarLookup.top().end()){
+        //for(int i=)
+        int t = runtimeVar[runtimeVarLookup.top()[cmd]].val;
+        int n = old_sp.top();
+        old_sp.pop();
+        int o = old_sp.top();
+        for(int i=n;i>o;i--){
+            runtimeVar.pop_back();
+        }
+        runtimeVarLookup.pop();
+        runtimeStack.push(t);
+        eip.pop();
+        return 0;
+    }
+    return 0;
+}
+int PcodeInterpreter::do_jmp(const string cmd){
+    if(labelMap.find(cmd)!=labelMap.end()){
+        int tar = labelMap[cmd];
+        cout << "jmp target:"<<tar<<endl;
+        //cout << code[tar][0]<<endl;
+        eip.top() = tar-1;
+    } else {
+        cout << cmd << " not found in label map."<<endl;
+        return -1;
+    }
+    return 0;
+}
+int PcodeInterpreter::do_jz(const string cmd){
+    if(runtimeStack.size()<1){
+        cout << "invalid runtime stack depth"<<endl;
+        return -1;
+    }
+    if(labelMap.find(cmd)==labelMap.end()){
+        cout << cmd << " not found in label map."<<endl;
+        return -1;
+    }
+    int z = runtimeStack.top();
+    runtimeStack.pop();
+    if(z==0){
+        int tar = labelMap[cmd];
+        eip.top() = tar-1;
+    } else {
+        return 0;
+    }
+    return 0;
+}
 int PcodeInterpreter::do_print(const string cmd){
     vector<string> info;
     SplitString(cmd,info,",");
     for(auto i:info){
         if(i.front()=='\"'&&i.back()=='\"'){
             cout << i.substr(1,i.length()-2);
-        } else if(runtimeVarLookup.find(i)!=runtimeVarLookup.end()){
-            for(int j=old_sp;j<runtimeVar.size();j++){
-                if(runtimeVar[j].name==i){//暂时不考虑变量未初始化的问题，默认是0
-                    cout << runtimeVar[j].val;
-                    break;
-                }
-            }
+        } else if(runtimeVarLookup.top().find(i)!=runtimeVarLookup.top().end()){
+            cout << runtimeVar[runtimeVarLookup.top()[i]].val;
         }
     }
-    //vector<string> output;
+    cout << endl;
     return 0;
+}
+int PcodeInterpreter::do_exit(const string cmd){
+    exit(0);
 }
 int PcodeInterpreter::do_input(const string cmd){
     //<r_stmt> ::= scanf'('<ident>{,<ident>}')'
@@ -69,8 +163,8 @@ int PcodeInterpreter::do_input(const string cmd){
     string msg;
     SplitString(cmd,vars,",");
     for(auto v:vars){
-        if(runtimeVarLookup.find(v)!=runtimeVarLookup.end()){
-            for(int i=old_sp;i<runtimeVar.size();i++){
+        if(runtimeVarLookup.top().find(v)!=runtimeVarLookup.top().end()){
+            for(int i=old_sp.top();i<runtimeVar.size();i++){
                 if(runtimeVar[i].name==v){//暂时不考虑变量未初始化的问题，默认是0
                     cin >> runtimeVar[i].val;
                     msg += "setting "+runtimeVar[i].name+" to "+to_string(runtimeVar[i].val);
@@ -116,21 +210,22 @@ int PcodeInterpreter::do_var(const string cmd){
     for(auto var:vars){
         vector<string> attr;
         SplitString(var,attr,":");
-        if(runtimeVarLookup.find(attr[0])!=runtimeVarLookup.end()){
+        if(runtimeVarLookup.top().find(attr[0])!=runtimeVarLookup.top().end()){
             cout << "duplicate definition of variable "<<var<<endl;
             return -1;
         } else {
-            runtimeVarLookup[attr[0]] = false;
-            runtimeVar.push_back({type:attr[1],name:attr[0]});
+            runtimeVarLookup.top()[attr[0]] = runtimeVar.size();
+            runtimeVar.push_back({attr[1],attr[0],0});
+            old_sp.top()++;
         }
     }
     cout << "cmd:"<<cmd << endl;
     return 0;
 }
-int PcodeInterpreter::do_push(const string cmd){
+int PcodeInterpreter::do_push(const string cmd){//如果发现是数组操作，去栈拿【i】：栈顶的是下标，运算数在第二个
     int a;
-    if(runtimeVarLookup.find(cmd)!=runtimeVarLookup.end()){//这是个变量名，去找变量值
-        for(int i=old_sp;i<runtimeVar.size();i++){
+    if(runtimeVarLookup.top().find(cmd)!=runtimeVarLookup.top().end()){//这是个变量名，去找变量值
+        for(int i=old_sp.top();i<runtimeVar.size();i++){
             if(runtimeVar[i].name==cmd){//暂时不考虑变量未初始化的问题，默认是0
                 a = runtimeVar[i].val;
                 cout << "variable detected:";
@@ -146,15 +241,15 @@ int PcodeInterpreter::do_push(const string cmd){
     cout << "push:"<<a << endl;
     return 0;
 }
-int PcodeInterpreter::do_pop(const string cmd){
+int PcodeInterpreter::do_pop(const string cmd){//如果发现是数组操作，去栈拿【i】：栈顶的是下标，运算数在第二个
     cout << "pop:"<<cmd << endl;
     if(cmd==""){//直接pop，不管了
         runtimeStack.pop();
         cout <<" direct pop"<<endl;
         return 0;
     }
-    if(runtimeVarLookup.find(cmd)!=runtimeVarLookup.end()){//这是个变量名，去找变量值
-        for(int i=old_sp;i<runtimeVar.size();i++){
+    if(runtimeVarLookup.top().find(cmd)!=runtimeVarLookup.top().end()){//这是个变量名，去找变量值
+        for(int i=old_sp.top();i<runtimeVar.size();i++){
             if(runtimeVar[i].name==cmd){
                 cout <<"indirect pop"<<endl;
                 runtimeVar[i].val = runtimeStack.top();
@@ -164,6 +259,17 @@ int PcodeInterpreter::do_pop(const string cmd){
         }
     }
     return -1;
+}
+int PcodeInterpreter::func_call(const string funcName){
+    string fname = funcName.substr(1,funcName.length());
+    if(funcMap.find(fname)!=funcMap.end()){
+        cout << fname<<" is not a valid funcname"<<endl;
+        return -1;
+    } else {
+        eip.push(funcMap[fname]);
+        old_sp.push(old_sp.top()+1);
+    }
+    return 0;
 }
 int PcodeInterpreter::interpret(const std::string &in_file_name){
     ifstream myfile(in_file_name);
@@ -194,12 +300,22 @@ int PcodeInterpreter::interpret(const std::string &in_file_name){
                 printf("duplicate funcname error");
                 return -1;
             }
-            this->funcMap.insert(make_pair(funcName,this->code.size()));//具体回退到哪还不确定
+            this->funcMap.insert(make_pair(funcName,this->code.size()));//回退到函数入口
         }
     }
     int i=0;
-    while(eip<code.size()&&i<12){
-        vector<string> cmd = code[eip];
+    while(i<12){
+        vector<string> cmd = code[eip.top()];
+        if(cmd.size()>0){
+            if(cmd[0].back()==':'){
+                i++;eip.top()++;
+                continue;
+            } else if(cmd[0].front()=='$'){
+                func_call(cmd[0]);
+                i++;eip.top()++;
+                continue;
+            }
+        }
         if(cmd.size()==2){
             (this->*cmdHandler[cmd[0]])(cmd[1]);
         } else {
@@ -211,7 +327,7 @@ int PcodeInterpreter::interpret(const std::string &in_file_name){
         //(this->*p)(cmd[1]);
         //break;
         i++;
-        eip++;
+        eip.top()++;
     }
     cout << "-labelMap---"<<endl;
     for(auto iter=labelMap.begin();iter!=labelMap.end();iter++){
--- compiler/interpreter.h
+++ compiler/interpreter.h
@@ -22,15 +22,16 @@ class PcodeInterpreter
 {
 private:
     std::vector<Variable> runtimeVar;//变量表：？
-    std::unordered_map<std::string,bool> runtimeVarLookup;
+    std::stack<std::unordered_map<std::string,int>> runtimeVarLookup;
     std::unordered_map<std::string,Variable> globalVar;//全局变量表
     std::stack<int> runtimeStack;//stack运算栈：stack,int
     std::unordered_map<std::string,int> labelMap;//label表：label名对应跳到第几行代码
     std::unordered_map<std::string,int> funcMap;//函数表：？指示函数ret到哪行 注意函数有返回值则会往栈里放东西
     std::vector<std::vector<std::string>> code;//code代码区：暂定pair
     std::unordered_map<std::string,int(PcodeInterpreter::*)(const std::string)> cmdHandler;
-    int eip;//eip指针：int
-    int old_sp;//old_Stack_pointer
+    //int eip;//eip指针：int
+    std::stack<int> eip;
+    std::stack<int> old_sp;
     int do_var(const std::string);
     int do_push(const std::string);
     int do_pop(const std::string);
@@ -38,8 +39,12 @@ private:
     int do_mul(const std::string);
     int do_print(const std::string);
     int do_input(const std::string);
-    
-    
+    int do_jmp(const std::string);
+    int do_jz(const std::string);
+    int do_exit(const std::string);
+    int do_ret(const std::string);
+    int do_arg(const std::string);
+    int func_call(const std::string);
 public:
     PcodeInterpreter();
     int interpret(const std::string &in_file_name);
